b20ce91843fd71faf7611f9dc36c988d65ad5fcf
AlvaroRevi <100405496@alumnos.uc3m.es>
2022-04-05 02:05:27 +0200

    Niquelao el code hasta las performances

--- Lab2/untitled.m
+++ Lab2/untitled.m
@@ -1,10 +1,17 @@
 clear; clc; close all; 
+%% Global parameters
+m = 0.72;  
+g = 9.806; 
+W = m*g;
+S = 0.34;
+rho = 1.225; 
+deltaH = 20; 
+
 %% Data processing
 % Headers of the raw data from XFLR5
 Headers = {'alpha','Beta','CL','CDi','CDv','CD','CY', 'Cl','Cm','Cn','Cni','QInf','XCP'};
 
-% Legend for the different cases of study 
-leyenda = {'$+2^{\circ}$','$-3^{\circ}$','$-4^{\circ}$','$-5^{\circ}$','$-6^{\circ}$','$-7^{\circ}$'};
+
 
 % Read one by one each of the .txt of the directory and store the data 
 F = dir('*.txt');
@@ -15,6 +22,7 @@ for ii = 1:length(F)
     alpha(:,ii) = data{ii}.alpha;
     Cm(:,ii) = data{ii}.Cm;
     CL(:,ii) = data{ii}.CL; 
+    CD(:,ii) = data{ii}.CD;
 end
 
 %% Obtain alpha effective
@@ -24,39 +32,80 @@ for i=2:length(alpha(1,:))
     alpha_0(i) = interp1(CL(:,i),alpha(:,i),0);
 end
 
+alpha_true = alpha - alpha_0; 
+
+%% Look for the alpha that gives Cm = 0
+
+alpha_zero_torque = interp1(Cm,alpha_true,0);
+
+Cl_zero_moment = interp1(alpha_true,CL,alpha_zero_torque);
+
+V = sqrt(2*W/(rho*S*Cl_zero_moment));
+
+%% Gliding Performance 
+
+Eff = CL./CD;
+
+Range = deltaH.*Eff(CL>=0);
+
+rate_descent = - sqrt(2*W/(rho*S)).*CD(CL>=0)./(CL(CL>=0).^(3/2)); 
+
+Endurance = -deltaH./rate_descent;
+
 %% Plots 
 figure(1)
 hold on 
-plot(alpha-alpha_0,Cm)
-yline(0,'--')
 grid minor
+plot(alpha-alpha_0,Cm,'LineWidth',1)
+yline(0,'--')
 axis square
 xlim([0,7.5])
 xlabel('$\alpha$ [rad]','Interpreter','latex')
 ylabel('$C_{m}$','Interpreter','latex')
-legend(leyenda,'Interpreter','latex')
-title('Analysis of the aircraft performance for different twist','Interpreter','latex')
+title('Analysis of the aircraft performance','Interpreter','latex')
 
 figure(2)
 hold on 
-plot(alpha-alpha_0,CL)
+grid minor 
+plot(alpha-alpha_0,CL,'LineWidth',1)
 yline(0,'--')
-grid minor
 axis square
-xlim([0,7.5])
+xlim([-3,7.5])
 xlabel('$\alpha$ [rad]','Interpreter','latex')
 ylabel('$C_{L}$','Interpreter','latex')
-legend(leyenda,'Interpreter','latex')
-title('Analysis of the aircraft performance for different twist','Interpreter','latex')
+title('Analysis of the aircraft performance','Interpreter','latex')
 
 figure(3)
 hold on 
-plot(CL,Cm)
+grid minor 
+plot(CL,Cm,'LineWidth',1)
 yline(0,'--')
 xlim([-0.2,0.6])
-grid minor
 axis square
 xlabel('$C_{L}$' ,'Interpreter','latex')
 ylabel('$C_{m}$','Interpreter','latex')
-legend(leyenda,'Interpreter','latex')
-title('Analysis of the aircraft performance for different twist','Interpreter','latex')
+title('Analysis of the aircraft performance','Interpreter','latex')
+
+figure(4)
+hold on 
+grid minor 
+plot(alpha-alpha_0,Eff,'LineWidth',1)
+yline(0,'--')
+xlim([-4,6])
+axis square
+xlabel('$\alpha$' ,'Interpreter','latex')
+ylabel('$C_{L}/C_{D}$','Interpreter','latex')
+title('Analysis of the aircraft performance','Interpreter','latex')
+
+figure(5)
+grid minor
+xlabel('Angle of attack $\alpha$ [$^{\circ}$]','Interpreter','latex','FontSize',18)
+title('Glider performance','Interpreter','latex','FontSize',18)
+
+yyaxis left 
+plot(alpha_true(CL>=0),Range)
+ylabel('Range [m]','Interpreter','latex','FontSize',18)
+
+yyaxis right 
+plot( alpha_true(CL>=0),Endurance)
+ylabel('Endurance [s]','Interpreter','latex','FontSize',18)
